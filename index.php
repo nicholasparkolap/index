08312698b25e7d5067210e495bf826944bf7125e
nicholasparkolap <nicholas.parkolap@daemen.edu>
2017-06-27 13:22:55 -0400

    Start
    This is the start of the index page that I am testing out.

---
+++ index.php
@@ -0,0 +1,28 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<style>
+
+	.w3-daemen-dark { color: white !important;background-color:#00294e !important }
+
+</style>
+
+<div class="w3-bar w3-daemen-dark" style="height: 50px; vertical-align: middle; " >
+
+	<a href="" class="w3-bar-item w3-right w3-mobile w3-button">
+		Login 
+	</a>
+	
+	<a href="https://dev.daemen.edu/administration/development/parkolap_test/index.php" class="w3-bar-item">
+		Home
+	</a>
+	
+	<a href="" class="w3-bar-item">
+		A to Z
+	</a>
+	
+	<a href="" class="w3-bar-item">
+		Search
+	</a>
+
+</div>
